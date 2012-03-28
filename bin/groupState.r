d7a40ef189e779a7fb9644ea9209b80d5e2cd13a
Dane Summers <dsummersl@yahoo.com>
2012-03-28 17:37:31 -0400

    grouping data a little better

--- bin/groupState.r
+++ bin/groupState.r
@@ -7,9 +7,13 @@ groups <- with(samples, ftable(
   SEX, 
   ANC, 
   cut(AGEP,breaks=c(0,18,25,30,35,40,50,60,Inf)),
-  cut(SCHL,breaks=c(1,12,Inf),labels=c('HS','College')),
-  cut(PINCP,breaks=c(seq(0,10,by=2)*10000,Inf))
+  cut(SCHL,breaks=c(1,12,17,Inf),labels=c('HS','College','Graduate')),
+  cut(PINCP,breaks=c(seq(0,20,by=2)*10000,Inf))
 ))
+
+# so I do this, to filter out the non-events:
 data <- as.data.frame(groups)
-data <- subset(data,Freq > 0) # exclude anything that doesn't actually have any values
+data <- subset(data,Freq > 0)
+colnames(data) <- c('Sex','Ancestry','Age','School','Income','IncomeCount')
+
 write.csv(data,file="out.csv",row.names=FALSE)
